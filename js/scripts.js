1cb752559395ec63e801d735ee91ef75a7c9c412
Tyson Lackey <lackeyt90@gmail.com>
2020-06-04 10:06:54 -0700

    add page elements based on age select
    co-authored-by: David Cole <davidtcole@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
 //Business Logic
 
 $(document).ready(function() {
-  $("form#Age").submit(function(event) {
+  $("form#age").submit(function(event) {
+
     const age = $("#ageSelect").val();
+    event.preventDefault()
+
+    if (age === "1") {
+      $("#over18").show();
+      $("#age").hide();
+    } else {
+      $("#under18").show();
+      $("#age").hide();
+    }
+
   });
 });
 
